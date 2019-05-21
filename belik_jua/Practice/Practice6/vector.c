74cc175370d8357e3ec860e6746ea920e416d031
BelikJulia <ybelik2015@yandex.ru>
2019-05-21 18:43:18 +0300

    Nightmare

--- belik_jua/Practice/Practice6/vector.c
+++ belik_jua/Practice/Practice6/vector.c
@@ -7,8 +7,8 @@
 void Print(Vector *v)
 {
     int i;
-    for (i = 0; i < (*v).n; i++)
-        printf("%lf ", (*v).x[i]);
+    for (i = 0; i < v->n; i++)
+        printf("%lf ", v->x[i]);
     printf("\n");
 }
 
@@ -16,9 +16,9 @@ Vector* Fill(Vector *v)
 {
     int i;
     printf("Enter coordinates \n");
-    for (i = 0; i < (*v).n; i++)
+    for (i = 0; i < v->n; i++)
     {
-        scanf("%lf", &((*v).x[i]));
+        scanf("%lf", &(v->x[i]));
     }
     return v;
 }
@@ -26,15 +26,15 @@ Vector* Fill(Vector *v)
 Vector* Cre(int _n)
 {
     Vector *A = (Vector*)malloc(sizeof(Vector));
-    (A)->n = _n;
-    (A)->x = (float*)malloc(sizeof(float) * _n);
+    A->n = _n;
+    A->x = (float*)malloc(sizeof(float) * _n);
     return A;
 }
 
 void DeleteAll(Vector *v)
 {
-    free((*v).x);
-    (*v).n = 0;
+    free (v->x);
+    v->n = 0;
 }
 
 Vector* Sum(Vector *v1, Vector *v2)
@@ -44,20 +44,20 @@ Vector* Sum(Vector *v1, Vector *v2)
     Vector *A;
     A = Cre(v1->n);
     int i;
-    for (i = 0; i < (*A).n; i++)
-        (*A).x[i] = (*v1).x[i] + (*v2).x[i];
+    for (i = 0; i < A->n; i++)
+        A->x[i] = v1->x[i] + v2->x[i];
     return A;
 }
 
 Vector* Diff(Vector *v1, Vector *v2)
 {
-    if ((*v1).n != (*v2).n)
+    if (v1->n != v2->n)
         return;
     Vector *A;
     A = Cre(v1->n);
     int i;
-    for (i = 0; i < (*A).n; i++)
-        (*A).x[i] = (*v1).x[i] - (*v2).x[i];
+    for (i = 0; i < A->n; i++)
+        A->x[i] = v1->x[i] - v2->x[i];
     return A;
 }
 
@@ -65,20 +65,20 @@ double Len(Vector *v)
 {
     double s = 0;
     int i;
-    for (i = 0; i < (*v).n; i++)
-        s += ((*v).x[i] * (*v).x[i]);
+    for (i = 0; i < v->n; i++)
+        s += (v->x[i] * v->x[i]);
     s = sqrt(s);
     return s;
 }
 
 double Skal(Vector *v1, Vector *v2)
 {
-    if ((*v1).n != (*v2).n)
+    if (v1->n != v2->n)
         return 0;
     double S = 0;
     int i;
-    for (i = 0; i < (v1)->n; i++)
-        S += ((v1)->x[i] * (v2)->x[i]);
+    for (i = 0; i < v1->n; i++)
+        S += (v1->x[i] * v2->x[i]);
     return S;
 }
 
